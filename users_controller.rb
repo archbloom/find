ec77e6d7e790449e6b48ceaf3fb1e9ba513f746a
Bhushan Authankar <bhushanauthankar@gmail.com>
2020-04-08 16:19:14 +0530

    Add response and handle validation - Invalid json, id required, not found

--- users_controller.rb
+++ users_controller.rb
@@ -3,19 +3,40 @@ require 'json'
 class UsersController
   def call(env)
     request = Rack::Request.new(env)
-    if env['PATH_INFO'] == ''
+    response = Rack::Response.new
+    response.headers['Content-Type'] = 'application/json'
+
+    if request.path_info == ''
       if request.post?
-        user = JSON.parse(request.body.read)
-        Database.add_user(user) # curl -X POST http://localhost:9292/users --data '{"id": 3, "name": "user three"}'
-        [200, {}, ['User added!']]
+        begin
+          user = JSON.parse(request.body.read)
+          if user['id'].nil?
+            response.write('ID is required')
+            response.status = 400
+          else
+            Database.add_user(user) # curl -X POST http://localhost:9292/users --data '{"id": 3, "name": "user three"}'
+            response.write({ message: 'User Added!'})
+          end
+        rescue JSON::ParserError
+          response.status = 400
+          response.write('Invalid JSON')
+        end
       else
-        [200, {}, [Database.get_users.to_s]]
+        response.write(JSON.generate(Database.get_users))
       end
-    elsif env['PATH_INFO'] =~ %r{/\d+}
-      id = env['PATH_INFO'].split('/').last.to_i
-      [200, {}, [Database.get_users(id).to_s]]
+    elsif request.path_info =~ %r{/\d+}
+      id = request.path_info.split('/').last.to_i
+      user = Database.get_users(id)
+      if user.empty?
+        response.write({ message: 'User not found' })
+      else
+        response.write(JSON.generate(user))
+      end
     else
-      [404, {}, ['Nothing to see here']]
+      response.status = 404
+      response.write('Nothing to see here')
     end
+
+    response.finish
   end
 end
